4444c73800c9199b913709bdf1ee70c4d7ebfabd
GoldFisch <hernandezcar11@icloud.com>
2025-02-11 20:15:57 -0500

    Update config.py

--- data/lib/config.py
+++ data/lib/config.py
@@ -1,17 +1,21 @@
 import ctypes
 import json
+
 from customtkinter import *
 from PIL import Image, ImageDraw
-import requests
-import os
 
-config_path = os.path.join(os.path.dirname(__file__), "config.json")
+import requests
+
+
+with open("path.txt", "r") as file:
+    config_path = f"{file.read()}\\data\\lib\\config.json"
 config_data = None
 
 def get_current_version():
     return read_config()["current_version"]
 
 def read_config(key=""):
+    # try:
     with open(config_path) as config_file:
         config_data = config_file.read()
         config_data = json.loads(config_data)
@@ -21,6 +25,8 @@ def read_config(key=""):
         if not key == "":
             return config_data[key]
         return config_data
+    # except:
+    #     ctypes.windll.user32.MessageBoxW(0, "CONFIG DATA ERROR!", "Error", 0)
 
 def save_config(config_data_p):
     global config_data
@@ -80,5 +86,56 @@ def save_tk_list(tk_var_list):
     config_data = read_config()
     iterate_save_dict(config_data, tk_var_list)
     save_config(config_data)
+
+def parent_path():
+    with open("path.txt", "r") as file:
+        return file.read()
+
+def round_corners(im, rad):
+    circle = Image.new('L', (rad * 2, rad * 2), 0)
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, rad * 2 - 1, rad * 2 - 1), fill=255)
+    alpha = Image.new('L', im.size, 255)
+    w, h = im.size
+    alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
+    alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
+    alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
+    alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
+    im.putalpha(alpha)
+    return im
+
+def theme_path():
+    if "/" in config_data["paths"]["theme"]:
+        return config_data["paths"]["theme"]
+    else:
+        return parent_path() + config_data["themes"][config_data["paths"]["theme"]]
+
+def read_theme(key=""):
+    with open(theme_path()) as theme_file:
+        theme_data = json.load(theme_file)
+        if len(theme_data) == 0:
+            ctypes.windll.user32.MessageBoxW(0, "THEME FILE NOT FOUND", "Error", 0)
+            exit(1)
+        if not key == "":
+            return theme_data[key]
+        return theme_data
+
+def read_json(path, key=""):
+    with open(f"{parent_path()}{path}") as file:
+        data = json.load(file)
+        if len(data) == 0:
+            ctypes.windll.user32.MessageBoxW(0, "JSON FILE NOT FOUND", "Error", 0)
+            exit(1)
+        if not key == "":
+            return data[key]
+        return data
+
+def save_theme_path(path):
+    config_data = read_config()
+    config_data["theme_path"] = path
+    save_config(config_data)
+
+def convert_to_ahk():
+    pass
 
 config_data = read_config()
